4b72ba63d09150b281f289ab43e2787d059e84d9
Shubham Jadhav <shub13711jadhav@gmail.com>
2021-11-08 16:25:16 +0530

    Update game.js

--- game.js
+++ game.js
@@ -127,7 +127,7 @@ scene("game", (stamina, score, currency, SPEED) => {
                 pos(200, height()),
                 origin("botleft"),
                 layer("top"),
-                scale(0.5), //for 100x100
+                scale(0.3), //for 100x100
                 //scale(0.5),//for 1080
                 area(),
                 body(),
