2848bd7bc592f8edc7f0139162c29a4d9fd6d1ae
Shubham Jadhav <shub13711jadhav@gmail.com>
2021-11-10 11:52:18 +0530

    Update game.js

--- game.js
+++ game.js
@@ -216,7 +216,7 @@ scene("game", (stamina, score, currency, SPEED) => {
                     //add bags 
             add([
                     sprite(choose(["bag1", "bag2", "bag3"])),
-                    pos(width()+rand(500, 600), height()),
+                    pos(width()+rand(500, 600), height()-20),
                     area(),
                     origin("botleft"),
                     layer("top"),
