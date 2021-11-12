9f726e7b464cedfc51f525ccc989b9c73824130c
Shubham Jadhav <shub13711jadhav@gmail.com>
2021-11-12 16:27:06 +0530

    Update game.js

--- game.js
+++ game.js
@@ -5,7 +5,7 @@ import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
  // initialize kaboom context
  kaboom({
     width: 1200,
-    height:400,
+    height:800,
     background: [ 255, 255, 255, ],
 });
 
