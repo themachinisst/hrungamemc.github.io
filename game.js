852916cb8918bebbb0e2418f3ae9be2d9842a0c9
Shubham Jadhav <shub13711jadhav@gmail.com>
2021-11-16 13:30:43 +0530

    Update game.js

--- game.js
+++ game.js
@@ -7,6 +7,7 @@ import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
     width: 800,
     height:400,
     background: [ 255, 255, 255, ],
+    touchToMouse: true,
 });
 
 //loadFont("MCFont", "./assets/Font/MCFont.png", 6, 8);
